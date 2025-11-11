bc7c35517e5a734c8dd41b1cdc0c41ca24c69b90
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-11-11 13:43:28 +0700

    demo

---
+++ backend.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {createClient} from '@supabase/supabase-js';
+import dotenv from 'dotenv'
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log("example");
+})
+
